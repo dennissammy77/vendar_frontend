c4e39e28d1e817dd416865f2c32e99a5c4b1f20f
dennissammy77 <dennissammy77@gmail.com>
2024-04-02 23:46:25 +0300

    Installed react query and created a query provider

--- src/components/providers/providers.js
+++ src/components/providers/providers.js
@@ -1,13 +1,20 @@
 'use client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { UserProvider } from './user.context';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 export function Providers({ children }) {
+  const queryClient = new QueryClient()
   return (
     <ChakraProvider>
-        <UserProvider>
-          {children}
-        </UserProvider>
+        <QueryClientProvider client={queryClient}>
+          <UserProvider>
+            {children}
+          </UserProvider>
+        </QueryClientProvider>
     </ChakraProvider>
   );
 }
