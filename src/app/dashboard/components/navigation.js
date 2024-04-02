c8207adc5676f449b8e542a2f605d77a494fa380
dennissammy77 <dennissammy77@gmail.com>
2024-04-02 12:16:29 +0300

    Routing update + user details display variable update

--- src/app/dashboard/components/navigation.js
+++ src/app/dashboard/components/navigation.js
@@ -53,10 +53,10 @@ const TopNav = ({ onOpen,onToggle, ...rest }) => {
           <Menu>
             <MenuButton py={2} transition="all 0.3s" _focus={{ boxShadow: 'none' }}>
               <HStack>
-                <Avatar size={'sm'} src={ 'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'} name={user?.data?.name}/>
+                <Avatar size={'sm'} src={ 'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'} name={user?.data?.data?.name}/>
                 <VStack display={{ base: 'none', md: 'flex' }} alignItems="flex-start" spacing="1px" ml="2">
-                  <Text fontSize="md">{user?.data?.name || '-'}</Text>
-                  <Badge fontSize="xs" color="#ffffff" bg='#4E2FD7'>{user?.data?.shop_admin_account_ref?.role}</Badge>
+                  <Text fontSize="md">{user?.data?.data?.name || '-'}</Text>
+                  <Badge fontSize="xs" color="#ffffff" bg='#4E2FD7'>{user?.data?.data?.shop_admin_account_ref?.role}</Badge>
                 </VStack>
                 <Box display={{ base: 'none', md: 'flex' }}>
                   {/**
@@ -105,7 +105,7 @@ const SidebarContent = ({onClose,navigation,display,width}) => {
       </Flex>
       {navigation?.map((item)=>{
         return(
-          <NavItem key={item?.id} bg={pathArr[2] === item?.title?.toLowerCase() ? '#E4F0FC' : '#FAFAFA'} color={pathArr[2] === item?.title?.toLowerCase() ? '#4E2FD7' : '#9298AC'} borderRadius={pathArr[2] ==- item?.title?.toLowerCase()? 'md' : '5'} icon={item?.icon} onClick={(()=>{router.push(item?.route);onClose()})}>
+          <NavItem key={item?.id} bg={pathArr[2] === item?.title?.toLowerCase() ? '#E4F0FC' : '#FAFAFA'} color={pathArr[2] === item?.title?.toLowerCase() ? '#4E2FD7' : '#9298AC'} borderRadius={pathArr[2] ==- item?.title?.toLowerCase()? 'md' : '5'} icon={item?.icon} onClick={(()=>{router.push(item?.route+'?uid='+user?.data?.data?._id);onClose()})}>
             {item.title}
           </NavItem>
         )
