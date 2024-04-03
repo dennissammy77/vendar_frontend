fdc678d0bf5613e9af1038b8ac1c2309ac5c45b3
dennissammy77 <dennissammy77@gmail.com>
2024-04-03 14:25:13 +0300

    Updated editing store deatils form

--- src/components/ui/store/EditStoreForm.js
+++ src/components/ui/store/EditStoreForm.js
@@ -1,20 +1,21 @@
 'use client'
 import { UserContext } from '@/components/providers/user.context';
-import { Button, FormControl, FormLabel, Input, Text, Textarea, useToast } from '@chakra-ui/react';
-import { usePathname, useRouter } from 'next/navigation';
+import { Button, Divider, FormControl, FormLabel, Icon, Input, InputGroup, InputLeftElement, Text, Textarea, useToast } from '@chakra-ui/react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import React, { useContext, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup";
-import { CreateNewStore } from '@/app/api/shop/route';
-import { FaWandMagicSparkles } from 'react-icons/fa6';
+import { UpdateStore } from '@/app/api/shop/route';
+import { FaInstagram, FaTiktok, FaWhatsapp } from "react-icons/fa";
+import { FaXTwitter } from 'react-icons/fa6';
 
-export default function EditStoreForm({store_data}) {
+export default function EditStoreForm(props) {
     const router = useRouter();
     const toast = useToast();
     const {user} = useContext(UserContext);
-    const pathname = usePathname();
-    const pathArr = pathname?.split('/');
+    
+    const data = props.store_data;
     const EmailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
     const schema = yup.object().shape({
@@ -23,12 +24,14 @@ export default function EditStoreForm({store_data}) {
         mobile: yup.string(),
         email: yup.string().email().matches(EmailRegex, 'Email address must be of correct format'),
         location: yup.string(),
-        shelves: yup.number().min(1).required('Number of shelves is required')
+        shelves: yup.number().min(1).required('Number of shelves is required'),
+        instagram_url: yup.string(),
+        twitter_url:   yup.string(),
+        tiktok_url:    yup.string(),
+        whatsapp_url:  yup.string(),
     });
     useEffect(()=>{
         router.prefetch(`/dashboard/home?uid=${user?.data?.data?._id}`);
-        console.log(pathArr)
-        console.log(store_data)
     },[]);
     const {
         register, 
@@ -38,35 +41,38 @@ export default function EditStoreForm({store_data}) {
     } = useForm({
         resolver: yupResolver(schema),
         defaultValues: {
-            name:         store_data?.name,
-            description:  store_data?.description,
-            mobile:       store_data?.mobile,
-            location:     store_data?.location,
-            email:        store_data?.email,
-            shelves:      store_data?.shelves,
+            name:        data?.name,
+            description: data?.description,
+            mobile:      data?.mobile,
+            location:    data?.location,
+            email:       data?.email,
+            shelves:     data?.shelves,
+            instagram_url: data?.instagram_url,
+            twitter_url:   data?.twitter_url,
+            tiktok_url:    data?.tiktok_url,
+            whatsapp_url:  data?.whatsapp_url,
         },
     });
 
     const onSubmit = async(data) => {
-        console.log(data)
+        const uid = user?.data?.data?._id;
+        const flag = 'details';
+        const store_id = props?.store_data?._id;
         try{
-            // await CreateNewStore(data).then((response)=>{
-            //     if(response?.data?.error){
-            //         return toast({ title: 'Error!', description: `${response?.data?.message || response?.response?.data.message}`, status: 'error', variant:'left-accent', position: 'top-left', isClosable: true });
-            //     }
-            //     toast({ title: 'Success!', description: `${response?.data?.message}`, status: 'success', variant:'left-accent', position: 'top-left', isClosable: true });
-            //     setTimeout(()=>{
-            //         if(pathArr.some(path => path === 'onboarding')){
-            //             router.push(`/dashboard/home?uid=${user?.data?.data?._id}`);
-            //         }
-            //         router.push(`/dashboard/stores?uid=${user?.data?.data?._id}`);
-            //     },2000)
-            //     return ;
-            // }).catch((err)=>{
-            //     return toast({ title: `Error`, description: `Could not create your store:${err}`, status: 'error', variant:'left-accent', position: 'top-left', isClosable: true });
-            // })
+            await UpdateStore(data,store_id,uid,flag).then((response)=>{
+                if(response?.data?.error){
+                    return toast({ title: 'Error!', description: `${response?.data?.message || response?.response?.data.message}`, status: 'error', variant:'left-accent', position: 'top-left', isClosable: true });
+                }
+                toast({ title: 'Success!', description: `${response?.data?.message}`, status: 'success', variant:'left-accent', position: 'top-left', isClosable: true });
+                setTimeout(()=>{
+                    router.push(`/dashboard/stores?uid=${user?.data?.data?._id}`);
+                },2000)
+                return ;
+            }).catch((err)=>{
+                return toast({ title: `Error`, description: `Could not update your store:${err}`, status: 'error', variant:'left-accent', position: 'top-left', isClosable: true });
+            })
         }catch(err){
-            return toast({ title: `Error`, description: `Could not create your store:${err}`, status: 'error', variant:'left-accent', position: 'top-left', isClosable: true });
+            return toast({ title: `Error`, description: `Could not update your store:${err}`, status: 'error', variant:'left-accent', position: 'top-left', isClosable: true });
         }
     }
   return (
@@ -100,6 +106,48 @@ export default function EditStoreForm({store_data}) {
             <FormLabel>About the store</FormLabel>
             <Textarea h='100px' disabled={isSubmitting} {...register('description')} type='text' placeholder='Tell us a bit about your store' variant='filled'/>
             {errors.description && ( <Text fontSize={'sm'} color='red'>{errors.description.message}</Text>)}
+        </FormControl>
+        <Text fontWeight={'bold'}>Socials</Text>
+        <Divider />
+        <FormControl mt='1' my='2'>
+            <FormLabel>Instagram</FormLabel>
+            <InputGroup>
+                <InputLeftElement pointerEvents='none'>
+                    <Icon boxSize={'4'} as={FaInstagram} color='' />
+                </InputLeftElement>
+                <Input disabled={isSubmitting} {...register('instagram_url')} type='url' placeholder='instagram social link' variant='filled'/>
+            </InputGroup>
+            {errors.instagram_url && ( <Text fontSize={'sm'} color='red'>{errors.instagram_url.message}</Text>)}
+        </FormControl>
+        <FormControl mt='1' my='2'>
+            <FormLabel>Twitter</FormLabel>
+            <InputGroup>
+                <InputLeftElement pointerEvents='none'>
+                    <Icon boxSize={'4'} as={FaXTwitter} color='' />
+                </InputLeftElement>
+                <Input disabled={isSubmitting} {...register('twitter_url')} type='url' placeholder='twitter social link' variant='filled'/>
+            </InputGroup>
+            {errors.twitter_url && ( <Text fontSize={'sm'} color='red'>{errors.twitter_url.message}</Text>)}
+        </FormControl>
+        <FormControl mt='1' my='2'>
+            <FormLabel>WhatsApp</FormLabel>
+            <InputGroup>
+                <InputLeftElement pointerEvents='none'>
+                    <Icon boxSize={'4'} as={FaWhatsapp} color='' />
+                </InputLeftElement>
+                <Input disabled={isSubmitting} {...register('whatsapp_url')} type='url' placeholder='whatsapp social link' variant='filled'/>
+            </InputGroup>
+            {errors.whatsapp_url && ( <Text fontSize={'sm'} color='red'>{errors.whatsapp_url.message}</Text>)}
+        </FormControl>
+        <FormControl mt='1' my='2'>
+            <FormLabel>Tiktok</FormLabel>
+            <InputGroup>
+                <InputLeftElement pointerEvents='none'>
+                    <Icon boxSize={'4'} as={FaTiktok} color='' />
+                </InputLeftElement>
+                <Input disabled={isSubmitting} {...register('tiktok_url')} type='url' placeholder='tiktok social link' variant='filled'/>
+            </InputGroup>
+            {errors.tiktok_url && ( <Text fontSize={'sm'} color='red'>{errors.tiktok_url.message}</Text>)}
         </FormControl>
         {isSubmitting?
             <Button isLoading loadingText='saving...' variant='ghost' borderRadius={'md'} w='full'/>
