adc0981c388d9eb138376622643cef8f49d80e67
dennissammy77 <dennissammy77@gmail.com>
2024-04-18 05:37:18 +0300

    update file

--- src/app/api/transaction/route.js
+++ src/app/api/transaction/route.js
@@ -2,7 +2,7 @@ import { Cookies } from "react-cookie";
 import RouteHandler from "../routehandler";
 import axios from "axios";
 
-export async function FETCH_STORE_TRANSACTIONS_DATA(store_id){
+export async function FETCH_STORE_TRANSACTIONS_DATA(user_id,store_id){
     let BASE_URL = await RouteHandler();
     const cookies = new Cookies();
     const AUTH_TOKEN = cookies.get('user_token1');
@@ -10,7 +10,7 @@ export async function FETCH_STORE_TRANSACTIONS_DATA(store_id){
     let config = {
         method: 'get',
         maxBodyLength: Infinity,
-        url: `${BASE_URL}/api/transaction/store?store_id=${store_id}`,
+        url: `${BASE_URL}/api/transaction/store?store_id=${store_id}&user_id=${user_id}`,
         headers: { 
             'Content-Type': 'application/json', 
             'Authorization': `Bearer ${AUTH_TOKEN}`
@@ -59,6 +59,30 @@ export async function NEW_STORE_TRANSACTION(data,USER_ID, PRODUCT_ID, STORE_ID){
         method: 'post',
         maxBodyLength: Infinity,
         url: `${BASE_URL}/api/transaction/new?user_id=${USER_ID}&product_id=${PRODUCT_ID}&store_id=${STORE_ID}`,
+        headers: { 
+            'Content-Type': 'application/json', 
+            'Authorization': `Bearer ${AUTH_TOKEN}`
+        },
+        data: data
+    };
+
+    const response = await axios.request(config).then((response) => {
+        return response;
+    }).catch((error) => {
+        return (error)
+    });
+    return response;
+}
+
+export async function UPDATE_STORE_TRANSACTION(data,USER_ID, PRODUCT_ID, TRANSACTION_ID){
+    let BASE_URL = await RouteHandler();
+    const cookies = new Cookies();
+    const AUTH_TOKEN = cookies.get('user_token1');
+
+    let config = {
+        method: 'put',
+        maxBodyLength: Infinity,
+        url: `${BASE_URL}/api/transaction/update?user_id=${USER_ID}&product_id=${PRODUCT_ID}&transaction_id=${TRANSACTION_ID}`,
         headers: { 
             'Content-Type': 'application/json', 
             'Authorization': `Bearer ${AUTH_TOKEN}`
