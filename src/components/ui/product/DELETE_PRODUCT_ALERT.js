fd033546679fc0f693862badd52d872a055a7304
dennissammy77 <dennissammy77@gmail.com>
2024-04-18 05:38:37 +0300

    update file

--- src/components/ui/product/DELETE_PRODUCT_ALERT.js
+++ src/components/ui/product/DELETE_PRODUCT_ALERT.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Button, AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogHeader, AlertDialogBody, AlertDialogFooter, useToast } from '@chakra-ui/react'
+import { Button, AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogHeader, AlertDialogBody, AlertDialogFooter, useToast, UnorderedList, ListItem, OrderedList, Badge, Text } from '@chakra-ui/react'
 import { useRouter } from 'next/navigation';
 import { DELETE_STORE_PRODUCT } from '@/app/api/product/route';
 
-export default function DELETE_PRODUCT_ALERT({isOpen, onClose, USER_ID, PRODUCT_ID}) {
+export default function DELETE_PRODUCT_ALERT({isOpen, onClose, USER_ID, PRODUCT_ID, PRODUCT_DATA}) {
     const toast = useToast();
     const router = useRouter();
 
@@ -37,7 +37,21 @@ export default function DELETE_PRODUCT_ALERT({isOpen, onClose, USER_ID, PRODUCT_
                     </AlertDialogHeader>
 
                     <AlertDialogBody>
-                        Are you sure? You can't undo this action afterwards.
+                        Are you sure? You can't undo this action afterwards.You will lose your product and the following information:
+                        <UnorderedList>
+                            <Text fontWeight={'bold'} fontSize={'lg'}>{PRODUCT_DATA?.transactions?.length} sales</Text>
+                            {PRODUCT_DATA?.transactions?.map((transaction)=>{
+                                return(
+                                    <ListItem>
+                                        <Text fontWeight='bold'>Total: </Text>
+                                        KES {transaction?.payment_total} 
+                                        <Badge colorScheme={transaction?.payment ? 'green':'orange'} ml='2'>
+                                            {transaction?.status}
+                                        </Badge>
+                                    </ListItem>
+                                )
+                            })}
+                        </UnorderedList>
                     </AlertDialogBody>
 
                     <AlertDialogFooter>
