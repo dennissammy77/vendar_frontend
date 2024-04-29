7f28c24999462eb609e02ca71d01ccf818e53794
Dennis Sammy <dennissammy77@gmail.com>
2024-04-29 23:02:04 +0300

    products Feature:

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -25,6 +25,29 @@ export async function NEW_STORE_PRODUCT (data,STORE_ID,USER_ID){
   });
   return response;
 }
+export async function MANY_NEW_STORE_PRODUCTS (data,STORE_ID,USER_ID){
+  let BASE_URL = await RouteHandler();
+  const cookies = new Cookies();
+  const AUTH_TOKEN = cookies.get('user_token1');
+  
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: `${BASE_URL}/api/product/many/new?user_id=${USER_ID}&store_id=${STORE_ID}`,
+    headers: { 
+      'Content-Type': 'application/json', 
+      'Authorization': `Bearer ${AUTH_TOKEN}`
+    },
+    data : data
+  };
+
+  const response = await axios.request(config).then((response) => {
+      return response;
+  }).catch((error) => {
+      return (error)
+  });
+  return response;
+}
 
 export async function UPDATE_STORE_PRODUCT (data,STORE_ID,USER_ID,PRODUCT_ID,FLAG){
   let BASE_URL = await RouteHandler();
@@ -114,4 +137,27 @@ export async function FETCH_PRODUCT_DATA (PRODUCT_ID){
         return(error)
       });
     return result;
+  }
+
+  export async function MOVE_STORE_PRODUCT_TO_STAKEHOLDER (STORE_ID,USER_ID,ACCOUNT_ID,PRODUCT_ID){
+    let BASE_URL = await RouteHandler();
+    const cookies = new Cookies();
+    const AUTH_TOKEN = cookies.get('user_token1');
+    
+    let config = {
+      method: 'put',
+      maxBodyLength: Infinity,
+      url: `${BASE_URL}/api/product/move?user_id=${USER_ID}&product_id=${PRODUCT_ID}&store_id=${STORE_ID}&account_id=${ACCOUNT_ID}`,
+      headers: { 
+        'Content-Type': 'application/json', 
+        'Authorization': `Bearer ${AUTH_TOKEN}`
+      }
+    };
+  
+    const response = await axios.request(config).then((response) => {
+        return response;
+    }).catch((error) => {
+        return (error)
+    });
+    return response;
   }
