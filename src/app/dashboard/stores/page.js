1187dd8cb0cdeffd7396a4d201f7a8ec02640998
Dennis Sammy <dennissammy77@gmail.com>
2024-04-30 22:06:44 +0300

    Icons: Imports

--- src/app/dashboard/stores/page.js
+++ src/app/dashboard/stores/page.js
@@ -1,26 +1,30 @@
 'use client'
-import { Badge, Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Button, Flex, Grid, GridItem, HStack, Icon, Input, InputGroup, InputLeftElement, Spinner, Text } from '@chakra-ui/react'
+
 import React, { useContext, useState } from 'react';
-import { IoMdAdd } from "react-icons/io";
-
-import { UserContext } from '@/components/providers/user.context';
-import { FaStore } from 'react-icons/fa';
+//styling
+import { Badge, Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Button, Flex, Grid, GridItem, HStack, Icon, Input, InputGroup, InputLeftElement, Spinner, Text } from '@chakra-ui/react'
+// util
 import { useRouter } from 'next/navigation';
-import { FiSearch } from "react-icons/fi";
-import StoreDetails from './components/StoreDetails';
+import { UserContext } from '@/components/providers/user.context';
 import { useQuery } from '@tanstack/react-query';
-import { FETCH_STORES_BY_OWNER } from '@/app/api/shop/route';
-import { MdChevronRight } from 'react-icons/md';
-
 import Cookies from 'universal-cookie';
+// icons
+import { ADD_ICON, CHEVRON_RIGHT_ICON, SEARCH_ICON, STORE_ICON } from '@/components/lib/constants/icons';
+// api
+import { FETCH_STORES_BY_OWNER } from '@/app/api/shop/route';
+//components
+import StoreDetails from './components/StoreDetails';
 
 function Page() {
-    const router = useRouter();
-
+    // utils
     const {user} = useContext(UserContext);
+    const router = useRouter();
+    const cookies = new Cookies();
+    // config
     const USER_ID = user?.data?.data?._id;
     const [search_query, set_search_query]=useState('');
-
+    const active_store = cookies.get('active_store') || user?.data?.data?.store_ref[0]?._id;
+    // Functions
     const {data, isLoading} = useQuery({
         queryKey: ['stores_data', {USER_ID}],
         queryFn: async () => {
@@ -42,13 +46,18 @@ function Page() {
                     return null;
             }
         }
-    })
+    });
+    // DATA
+    const stores = data?.data?.data || user?.data?.data?.store_ref;
 
-    const cookies = new Cookies();
-
-    const active_store = cookies.get('active_store') || user?.data?.data?.store_ref[0]?._id;
-
-    const [selected_active_store, set_selected_active_store] = useState(null || data?.data?.data[0]);
+    if (isLoading){
+        return (
+          <Flex flexDirection={'column'} justifyContent={'center'} align='center' h='60vh'>
+              <Spinner />
+              <Text fontSize={'md'} fontWeight={'bold'} color='gray.300' my='2'>Fetching your stores...</Text>
+          </Flex>
+          )
+      }
     
     if (data?.data?.error){
         return (
@@ -58,7 +67,15 @@ function Page() {
         )
     }
 
-    const stores = data?.data?.data || user?.data?.data?.store_ref;
+    if (stores?.length === 0){
+        return (
+            <Flex border='1px solid' borderColor='#E4F0FC' borderRadius={'md'} boxShadow={'sm'} p='10' h='100%' justify={'center'} alignItems={'center'} textAlign={'center'} color='gray.300' fontWeight={'bold'} flexDirection={'column'} w='100%' my='4'>
+                <Icon as={STORE_ICON} boxSize={'6'}/>
+                <Text>Create a<Text color='#4E2FD7' textDecoration={'1px solid underline'} cursor={'pointer'} onClick={(()=>{router.push(`/dashboard/stores/new?uid=${user?.data?.data?._id}`)})}>New store</Text>to start managing clients, products.</Text>
+            </Flex>
+        )
+    }
+
     return (
         <Box>
             <Flex justify={'space-between'} align={{base:'',lg:'center'}} flexDirection={{base:'column',lg:'row'}}>
@@ -67,84 +84,30 @@ function Page() {
                     <Flex align='center' >
                         <InputGroup>
                             <InputLeftElement pointerEvents='none'>
-                                <Icon as={FiSearch} color='gray.500' ml='2'/>
+                                <Icon as={SEARCH_ICON} color='gray.500' ml='2'/>
                             </InputLeftElement>
                             <Input type='search' placeholder={'Search stores'} mx='2' onChange={((e)=>{set_search_query(e.target.value)})}/>
                         </InputGroup>
-                        <Button variant={'filled'} borderRadius={'md'} bg='#4E2FD7' color='#fff' leftIcon={<IoMdAdd />} onClick={(()=>{router.push(`/dashboard/stores/new?uid=${user?.data?.data?._id}`)})}>New</Button>
+                        <Button variant={'filled'} borderRadius={'md'} bg='#4E2FD7' color='#fff' leftIcon={<ADD_ICON />} onClick={(()=>{router.push(`/dashboard/stores/new?uid=${user?.data?.data?._id}`)})}>New</Button>
                     </Flex>
                 }
             </Flex>
-            <Breadcrumb spacing='8px' separator={<MdChevronRight color='gray.500' />} my='2'>
+            <Breadcrumb spacing='8px' separator={<CHEVRON_RIGHT_ICON color='gray.500' />} my='2'>
                 <BreadcrumbItem>
-                    <BreadcrumbLink href={`/dashboard/home/?uid=${user?.data?.data?._id}/store_id=${selected_active_store}`}>Home</BreadcrumbLink>
+                    <BreadcrumbLink href={`/dashboard/home/?uid=${user?.data?.data?._id}&store_id=${active_store}`}>Home</BreadcrumbLink>
                 </BreadcrumbItem>
 
                 <BreadcrumbItem isCurrentPage>
                     <BreadcrumbLink fontSize={'sm'} color='gray.400' fontWeight={'bold'}>Stores</BreadcrumbLink>
                 </BreadcrumbItem>
             </Breadcrumb>
-            {isLoading?
-                <Flex flexDirection={'column'} justifyContent={'center'} align='center' h='60vh'>
-                    <Spinner />
-                    <Text fontSize={'md'} fontWeight={'bold'} color='gray.300' my='2'>Fetching your stores...</Text>
-                </Flex> 
-            :
-            <>
-                {stores?.length === 0? 
-                        <Flex border='1px solid' borderColor='#E4F0FC' borderRadius={'md'} boxShadow={'sm'} p='10' h='100%' justify={'center'} alignItems={'center'} textAlign={'center'} color='gray.300' fontWeight={'bold'} flexDirection={'column'} w='100%' my='4'>
-                            <Icon as={FaStore} boxSize={'6'}/>
-                            <Text>Create a<Text color='#4E2FD7' textDecoration={'1px solid underline'} cursor={'pointer'} onClick={(()=>{router.push(`/dashboard/stores/new?uid=${user?.data?.data?._id}`)})}>New store</Text>to start managing clients, products.</Text>
-                        </Flex>
-                    :
-                    <Grid
-                        templateRows={{base:'repeat(2, 1fr)',md:'repeat(1, 1fr)'}}
-                        templateColumns={{base:'repeat(1, 1fr)',md:'repeat(2, 1fr)'}}
-                        gap={4}
-                        h='200px'
-                        my='2'
-                    >
-                        <GridItem p='' borderRadius={'5'}>
-                            <Box>
-                                {stores?.filter((store)=>store?.name?.toLowerCase().includes(search_query?.toLowerCase()))?.map((store,index)=>{
-                                    return(
-                                        <StoreItem store={store} index={index} key={store?._id} set_selected_active_store={set_selected_active_store} active_store={active_store}/>
-                                    )
-                                })}
-                            </Box>
-                        </GridItem>
-                        <GridItem>
-                            {selected_active_store ? 
-                                <StoreDetails store={selected_active_store} active_store={active_store}/>
-                                :
-                                <Flex border='1px solid' borderColor='#E4F0FC' borderRadius={'md'} boxShadow={'sm'} p='4' h='100%' justify={'center'} alignItems={'center'} textAlign={'center'} color='gray.300' fontWeight={'bold'} flexDirection={'column'}>
-                                    <Icon as={FaStore} boxSize={'6'}/>
-                                    <Text>Select a store to <br/>view details</Text>
-                                </Flex>
-                            }
-                        </GridItem>
-                    </Grid>
-                }
-            
-            </>
-            }
+            {stores?.filter((store)=>store?.name?.toLowerCase().includes(search_query?.toLowerCase()))?.map((store,index)=>{
+                return(
+                    <StoreDetails store={store} active_store={active_store}/>
+                )
+            })}
         </Box>
     )
 }
 
 export default Page;
-
-const StoreItem = ({store, set_selected_active_store,active_store}) =>{
-    return(
-        <HStack align='center' bg='#E4F0FC' borderRadius={'md'} mb='2' p='4' spacing='4' cursor={'pointer'} border={'2px solid'} borderColor={'gray.300'} onClick={(()=>{set_selected_active_store(store)})}>
-            <Icon as={FaStore} boxSize={'4'} color='gray.500'/>
-            <Box>
-                <Text fontWeight={'semibold'}>{store.name}</Text>
-                <HStack>
-                    <Badge fontSize="xs" colorScheme={store?.payment_plan === 'free'? 'orange' : 'green'}>{store?.payment_plan}</Badge>
-                    {active_store === store?._id ? <Badge fontSize="xs" colorScheme={'purple'}>active</Badge> : null }
-                </HStack>
-            </Box>
-        </HStack>
-    )
-}
