112124af3637f79222471a5893982f21cdf8ba95
Dennis Sammy <dennissammy77@gmail.com>
2024-04-30 22:09:03 +0300

    Loading state on delete modal button

--- src/components/ui/product/DELETE_PRODUCT_ALERT.js
+++ src/components/ui/product/DELETE_PRODUCT_ALERT.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogHeader, AlertDialogBody, AlertDialogFooter, useToast, UnorderedList, ListItem, OrderedList, Badge, Text } from '@chakra-ui/react'
 import { useRouter } from 'next/navigation';
 import { DELETE_STORE_PRODUCT } from '@/app/api/product/route';
@@ -6,20 +6,22 @@ import { DELETE_STORE_PRODUCT } from '@/app/api/product/route';
 export default function DELETE_PRODUCT_ALERT({isOpen, onClose, USER_ID, PRODUCT_ID, PRODUCT_DATA}) {
     const toast = useToast();
     const router = useRouter();
+    const [isSubmitting,set_isSubmitting]=useState(false)
 
     const HANDLE_SUBMIT = async()=>{
+        set_isSubmitting(true);
         await DELETE_STORE_PRODUCT(USER_ID, PRODUCT_ID).then((response)=>{
             if(response?.data?.error || response?.response?.data?.error){
                 return toast({ title: 'Error!', description: `${response?.data?.message || response?.response?.data.message}`, status: 'error', variant:'left-accent', position: 'top-left', isClosable: true });
             }
             toast({ title: 'Success!', description: `${response?.data?.message}`, status: 'success', variant:'left-accent', position: 'top-left', isClosable: true });
-            setTimeout(()=>{
-                onClose();
-                router.back()
-            },5000)
+            onClose();
+            router.back()
             return ;
         }).catch((err)=>{
             return toast({ title: `Error`, description: `Could not delete this product:${err}`, status: 'error', variant:'left-accent', position: 'top-left', isClosable: true });
+        }).finally(()=>{
+            set_isSubmitting(false);
         });
     }
 
@@ -58,9 +60,11 @@ export default function DELETE_PRODUCT_ALERT({isOpen, onClose, USER_ID, PRODUCT_
                         <Button onClick={onClose}>
                             Cancel
                         </Button>
-                        <Button colorScheme='red' onClick={HANDLE_SUBMIT} ml={3}>
-                            Delete
-                        </Button>
+                        {isSubmitting?
+                            <Button isLoading loadingText='deleting your product' variant='ghost' borderRadius={'md'} w='full'/>
+                        :
+                            <Button colorScheme='red' onClick={HANDLE_SUBMIT} ml={3}> Delete </Button>
+                        }
                     </AlertDialogFooter>
                 </AlertDialogContent>
             </AlertDialogOverlay>
