77db8569adb5a3830eb4e3128999f5faed023599
Dennis Sammy <dennissammy77@gmail.com>
2024-05-11 21:09:11 +0300

    Adding many new vendors accounts: UI Development

--- src/app/dashboard/vendors/page.js
+++ src/app/dashboard/vendors/page.js
@@ -1,43 +1,79 @@
 'use client'
-import { Box, Button, Text, Flex, Spinner, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Divider, HStack, Avatar, Icon, InputGroup, InputLeftElement, Input, Tag, TagLabel, TagLeftIcon, TableContainer, Table, Thead, Tr, Th, Tbody, Td, Badge } from '@chakra-ui/react'
+import { Box, Button, Text, Flex, Spinner, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Divider, HStack, Avatar, Icon, InputGroup, InputLeftElement, Input, Tag, TagLabel, TagLeftIcon, TableContainer, Table, Thead, Tr, Th, Tbody, Td, Badge, IconButton, useToast } from '@chakra-ui/react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import React, { useContext, useState } from 'react';
 
 import { useQuery } from '@tanstack/react-query';
-import { FETCH_STAKEHOLDERS_DATA } from '@/app/api/auth/route';
+import { FETCH_ALL_STAKEHOLDERS_DATA_FOR_EXPORT, FETCH_STAKEHOLDERS_DATA } from '@/app/api/auth/route';
 import { UserContext } from '@/components/providers/user.context';
-import { MdChevronRight } from 'react-icons/md';
-import { IoMdAdd, IoMdSettings } from 'react-icons/io';
-import { FiSearch } from 'react-icons/fi';
-import { FaStore } from 'react-icons/fa';
-import Cookies from 'universal-cookie';
+import { ADD_ICON, CHEVRON_LEFT_ICON, CHEVRON_RIGHT_ICON, EXPORT_ICON, FILTER_ICON, FILTER_ICON_CLOSE, IMPORT_ICON, MANAGE_ICON, PEOPLE_ICON, SEARCH_ICON } from '@/components/lib/constants/icons';
+import { PRIMARY_BRAND, TERTIARY_BRAND } from '@/components/lib/constants/theme';
+import { FETCH_ACTIVE_STORE_ID } from '@/components/hooks/SELECT_ACTIVE_STORE';
+import Link from 'next/link';
+import FAILED_DATA_REQUEST from '@/components/ui/handlers/failed.data.error';
+import { EXPORT_VENDORS_EXCEL } from '@/components/hooks/export/EXCEL';
 
 
 export default function Page() {
     const router = useRouter();
+    const toast = useToast();
+
     const {user} = useContext(UserContext);
+    const searchParams = useSearchParams();
 
-    const [search_query, set_search_query]=useState('')
+    //filter options
+    const [search_query, set_search_query]=useState('');
+    const [page,set_page] = useState(1);
 
-
-    const cookies = new Cookies();
-
-    const STORE_ID = cookies.get('active_store') || user?.data?.data?.store_ref[0]?._id;
+    // config 
+    const STORE_ID = FETCH_ACTIVE_STORE_ID() || searchParams.get('store_id');
+    const USER_ID = user?.data?.data?._id;
     const ACCOUNT_TYPE = 'vendor';
 
+    // Functions
     const {data, isLoading} = useQuery({
-        queryKey: ['stakeholders', {STORE_ID}],
-        queryFn: () => FETCH_STAKEHOLDERS_DATA(STORE_ID,ACCOUNT_TYPE)
+        queryKey: ['stakeholders', {STORE_ID,page,search_query}],
+        queryFn: () => FETCH_STAKEHOLDERS_DATA(STORE_ID,ACCOUNT_TYPE,page,search_query)
     });
 
     const VENDORS_DATA = data?.data?.data;
+    const VENDORS_COUNT = data?.data?.count;
+
+
+    const HANDLE_PAGE_CHANGE=(sign)=>{
+        if (page === 1 && sign === '-'){
+            set_page(1)
+            return;
+        }
+        switch (sign) {
+            case '+':
+                set_page(page + 1)
+                break;
+            case '-':
+                set_page(page - 1)
+                break;
+            default:
+                set_page(1)
+                break;
+        }
+    }
+
+    const HANDLE_EXPORT_DATA = async()=>{
+        const ALL_VENDORS = await FETCH_ALL_STAKEHOLDERS_DATA_FOR_EXPORT (STORE_ID,ACCOUNT_TYPE,USER_ID);
+        toast({ title: 'Exporting Vendors Data', description: `Please wait`, status: 'loading', variant: 'left-accent', position:'top-left',isClosable: true, duration: 1000 });
+        await EXPORT_VENDORS_EXCEL(ALL_VENDORS?.data?.data)
+        .then(()=>{
+            toast({ title: 'Success!', description: `Vendors exported successfully`, status: 'success', variant: 'left-accent', position:'top-left',isClosable: true });
+            return;
+        }).catch((err)=>{
+            toast({ title: `${err}`, description:``, status:'error', variant: 'left-accent', position: 'top-left', isClosable: true })
+            throw new Error('Error')
+        })
+    }
 
     if (data?.data?.error){
         return (
-            <Flex flexDirection={'column'} justifyContent={'center'} align='center' h='60vh'>
-                <Text fontSize={'large'} fontWeight={'bold'} color='gray.400' my='2'>Error occured fetching store users</Text>
-                <Text fontSize={'large'} fontWeight={'bold'} color='gray.400' my='2'>{data?.data?.message}</Text>
-            </Flex>
+            <FAILED_DATA_REQUEST message={data?.data?.message}/>
         )
     }
 
@@ -49,31 +85,62 @@ export default function Page() {
                     <Flex align='center' >
                         <InputGroup>
                             <InputLeftElement pointerEvents='none'>
-                                <Icon as={FiSearch} color='gray.500' ml='2'/>
+                                <Icon as={SEARCH_ICON} color='gray.500' ml='2'/>
                             </InputLeftElement>
                             <Input type='search' placeholder={'Search vendors'} mx='2' onChange={((e)=>{set_search_query(e.target.value)})}/>
                         </InputGroup>
-                        <Button bgColor={'#4E2FD7'} color='#ffffff' leftIcon={<IoMdAdd />} onClick={(()=>{router.push(`/dashboard/vendors/new?uid=${user?.data?.data?._id}&&store_id=${STORE_ID}`)})}>New</Button>
+                        <Link href={`/dashboard/vendors/new?uid=${USER_ID}&store_id=${STORE_ID}`}>
+                            <Button bgColor={PRIMARY_BRAND} color='#ffffff' leftIcon={<ADD_ICON />}>New</Button>
+                        </Link>
                     </Flex>
                 }
             </Flex>
-            <Breadcrumb spacing='8px' separator={<MdChevronRight color='gray.500' />} my='2'>
+            <Breadcrumb spacing='8px' separator={<CHEVRON_RIGHT_ICON color='gray.500' />} my='2'>
                 <BreadcrumbItem>
-                    <BreadcrumbLink href={`/dashboard/home/?uid=${user?.data?.data?._id}`}>Home</BreadcrumbLink>
+                    <BreadcrumbLink href={`/dashboard/home/?uid=${USER_ID}&store_id=${STORE_ID}`}>Home</BreadcrumbLink>
                 </BreadcrumbItem>
 
                 <BreadcrumbItem isCurrentPage>
                     <BreadcrumbLink fontSize={'sm'} color='gray.400' fontWeight={'bold'}>Vendors</BreadcrumbLink>
                 </BreadcrumbItem>
             </Breadcrumb>
-            {isLoading?
-                <Flex flexDirection={'column'} justifyContent={'center'} align='center' h='60vh'>
-                    <Spinner />
-                    <Text fontSize={'md'} fontWeight={'bold'} color='gray.300' my='2'>Fetching store users</Text>
+            <Box my='2' boxShadow={'md'} p='2'>
+                <Flex align='center' justify={'flex-end'}>
+                    <Flex gap='4' mx='2'>
+                        <Link href={`/dashboard/vendors/new/import?uid=${USER_ID}&store_id=${STORE_ID}`}>
+                            <HStack align='center' _hover={{bg:'gray.100'}} p='2' borderRadius={'5'} transition={'.3s ease-out'}>
+                                <Icon as={IMPORT_ICON} boxSize={'4'}/>
+                                <Text fontWeight={'bold'} fontSize={'sm'}>import</Text>
+                            </HStack>
+                        </Link>
+                        <HStack onClick={HANDLE_EXPORT_DATA} align='center' _hover={{bg:'gray.100'}} p='2' borderRadius={'5'} transition={'.3s ease-out'}>
+                            <Icon as={EXPORT_ICON} boxSize={'4'}/>
+                            <Text fontWeight={'bold'} fontSize={'sm'}>export</Text>
+                        </HStack>
+                    </Flex>
                 </Flex>
+            </Box>
+            <HStack align='center'>
+                <HStack align='center' spacing='2' color='gray.600'>
+                    <Icon as={CHEVRON_LEFT_ICON} boxSize={'4'} cursor='pointer' onClick={(()=>HANDLE_PAGE_CHANGE('-'))}/>
+                    <Text fontSize={'xs'}>{page}</Text>
+                    <Icon as={CHEVRON_RIGHT_ICON} boxSize={'4'} cursor='pointer' onClick={(()=>HANDLE_PAGE_CHANGE('+'))}/>
+                </HStack>
+                <Text fontSize={'xs'} my='2' color='gray.400' pl='2' borderRight={'1px solid'} borderRightColor={TERTIARY_BRAND}>showing {VENDORS_DATA?.length || 0}  of {VENDORS_COUNT? VENDORS_COUNT : 0} items</Text>
+            </HStack>
+            {VENDORS_DATA?.length === 0? 
+                    <Flex border='1px solid' borderColor='#E4F0FC' borderRadius={'md'} boxShadow={'sm'} p='10' h='60vh' justify={'center'} alignItems={'center'} textAlign={'center'} color='gray.300' fontWeight={'bold'} flexDirection={'column'} w='100%' my='4'>
+                        <Icon as={PEOPLE_ICON} boxSize={'6'}/>
+                        <Text>No vendors found!.</Text>
+                    </Flex>
                 :
-                <>
-                    <TableContainer boxShadow={'md'}>
+                <TableContainer boxShadow={'md'}>
+                    {isLoading?
+                        <Flex flexDirection={'column'} justifyContent={'center'} align='center' h='60vh'>
+                            <Spinner />
+                            <Text fontSize={'md'} fontWeight={'bold'} color='gray.300' my='2'>Fetching vendors</Text>
+                        </Flex>
+                        :
                         <Table variant='simple'>
                             <Thead bg='#E4F0FC'>
                                 <Tr>
@@ -84,7 +151,7 @@ export default function Page() {
                                 </Tr>
                             </Thead>
                             <Tbody>
-                                {VENDORS_DATA?.filter((vendor)=>vendor?.name?.toLowerCase().includes(search_query?.toLowerCase()))?.map((vendor)=>{
+                                {VENDORS_DATA?.map((vendor)=>{
                                     return(
                                         <Tr key={vendor?._id} >
                                             <Td>
@@ -99,12 +166,12 @@ export default function Page() {
                                             <Td>{vendor?.mobile}</Td>
                                             <Td><Badge colorScheme={vendor?.account_status_ref?.suspension_status ? 'orange':'green'}>{vendor?.account_status_ref?.suspension_status ? 'suspended' : 'active'}</Badge></Td>
                                             <Td>
-                                                {user?.data?.data?._id === vendor?._id ? 
+                                                {USER_ID === vendor?._id ? 
                                                     null
                                                     :
-                                                    <HStack color='gray.600' cursor={'pointer'}pr='1' onClick={(()=>{router.push(`/dashboard/vendors/view?uid=${user?.data?.data?._id}&store_id=${STORE_ID}&account_id=${vendor?._id}`)})}>
+                                                    <HStack color='gray.600' cursor={'pointer'}pr='1' onClick={(()=>{router.push(`/dashboard/vendors/view?uid=${USER_ID}&store_id=${STORE_ID}&account_id=${vendor?._id}`)})}>
                                                         <Text fontSize={'xs'} fontWeight={'bold'}>manage</Text>
-                                                        <Icon boxSize='4' as={IoMdSettings } cursor='pointer'/>
+                                                        <Icon boxSize='4' as={MANAGE_ICON } cursor='pointer'/>
                                                     </HStack>
                                                 }
                                             </Td>
@@ -113,35 +180,9 @@ export default function Page() {
                                 }
                             </Tbody>
                         </Table>
-                    </TableContainer>
-                </>
+                    }                    
+                </TableContainer>
             }
         </Box>
     )
 }
-
-const VendorItem=({vendor,STORE_ID})=>{
-    const router = useRouter();
-    const {user} = useContext(UserContext)
-  
-    return(
-      <Flex p='2' borderRadius={'5'} align={'center'} justify={'space-between'} transition={'.3s ease-in-out'} _hover={{bg:'gray.100'}} cursor={'pointer'}>
-        <HStack spacing='2' >
-          <Avatar name={vendor?.name} size='md' src={vendor?.profile_image_url}/>
-          <Box>
-            <Text fontSize={'sm'} fontWeight={'bold'}>{vendor?.name}</Text>
-            <Text fontSize={'xs'} color='gray.400' my='1'>{vendor?.email}</Text>
-            <Text fontSize={'xs'} color='gray.400'>{vendor?.mobile}</Text>
-          </Box>
-        </HStack>
-        {user?.data?.data?._id === vendor?._id ? 
-          null
-          :
-          <HStack color='gray.600' cursor={'pointer'}pr='1' onClick={(()=>{router.push(`/dashboard/vendors/view?uid=${user?.data?.data?._id}&store_id=${STORE_ID}&account_id=${vendor?._id}`)})}>
-              <Text fontSize={'xs'} fontWeight={'bold'}>manage</Text>
-              <Icon boxSize='4' as={IoMdSettings } cursor='pointer'/>
-          </HStack>
-        }
-      </Flex>
-    )
-  }
