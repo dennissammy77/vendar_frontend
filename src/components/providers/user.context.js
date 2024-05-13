3c27ea196dfff0785700bc22acea28acc0963d10
Dennis Sammy <dennissammy77@gmail.com>
2024-05-13 23:30:31 +0300

    Changes

--- src/components/providers/user.context.js
+++ src/components/providers/user.context.js
@@ -2,6 +2,7 @@ import { createContext, useContext, useEffect, useState } from 'react';
 import useFetchToken from '../hooks/useFetchToken.hook';
 import { FETCH_USER_DATA } from '@/app/api/auth/route';
 import { useQuery } from '@tanstack/react-query';
+import SELECT_ACTIVE_STORE from '../hooks/SELECT_ACTIVE_STORE';
 
 
 export const UserContext = createContext(null);
@@ -15,13 +16,14 @@ export function UserProvider({children}) {
     const USER_ID = RETREIVED_TOKEN?.sub || undefined;
 
     const {data, isLoading} = useQuery({
-        queryKey: ['USER_DATA',{USER_ID}],
+        queryKey: ['USER_DATA',{USER_ID,user_handler}],
         queryFn: async () => {
           try{
             if(USER_ID === undefined){
               set_user(null)
             }
             const result = await FETCH_USER_DATA(USER_ID);
+            SELECT_ACTIVE_STORE(result?.data?.data.active_store_ref?._id)
             set_user(result)
           }catch(error){
             throw new Error('Error while fetching user data',error)
