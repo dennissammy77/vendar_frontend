13418c4bd1a517a3512ef573f7250df0502f3b7b
Dennis Sammy <dennissammy77@gmail.com>
2024-05-15 20:35:01 +0300

    Changes

--- src/app/page.js
+++ src/app/page.js
@@ -9,11 +9,14 @@ import useLogOut from "@/components/hooks/useLogOut.hook";
 import Link from "next/link";
 import Features from "./(Home)/Features/page";
 import useFetchToken from "@/components/hooks/useFetchToken.hook";
+import { FETCH_ACTIVE_STORE_ID } from "@/components/hooks/SELECT_ACTIVE_STORE";
 
 export default function Index() {
   const router = useRouter();
   const toast = useToast()
   const {user} = useContext(UserContext);
+  const STORE_ID = FETCH_ACTIVE_STORE_ID();
+
   const token = useFetchToken();
   useEffect(()=>{
     router.prefetch('/signin')
@@ -46,17 +49,19 @@ export default function Index() {
               <Link href={`/signin`} >
                 <Text cursor={'pointer'} _hover={{fontWeight:'bold'}} transition={'.3s ease-in-out'}>SignIn</Text>
               </Link>
-              <Button as="a" size={'sm'} variant="solid" bgColor='#4E2FD7' color='#fff' display="inline-flex" alignItems="center" justifyContent="center" w={{ base: "full", sm: "auto", }} mr='1' cursor="pointer"  onClick={(()=>{router.push('/signup')})}> 
-                  Get Started
-                  <Icon boxSize={4} ml={1} viewBox="0 0 20 20" fill="currentColor" hideBelow={'sm'}>
-                      <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
-                  </Icon>
-              </Button>
+              <Link href={`/signup`} >
+                <Button size={'sm'} variant="solid" bgColor='#4E2FD7' color='#fff' display="inline-flex" alignItems="center" justifyContent="center" w={{ base: "full", sm: "auto", }} mr='1' cursor="pointer"> 
+                    Get Started
+                    <Icon boxSize={4} ml={1} viewBox="0 0 20 20" fill="currentColor" hideBelow={'sm'}>
+                        <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
+                    </Icon>
+                </Button>
+              </Link>
             </HStack>
             :
             <Flex align='center' mr='2' gap='2'>
               <Link onClick={(()=>{HandleLogout()})} href='/' cursor={'pointer'} color='#4E2FD7' fontWeight={'bold'}>LogOut</Link>
-              <Link href={`/dashboard/home?uid=${user?.data?.data?._id}&store_id=${user?.data?.data?.store_ref[0]?._id}`}>
+              <Link href={`/dashboard/home?uid=${user?.data?.data?._id}&store_id=${STORE_ID}`}>
                 <HStack align='center' spacing='2' fontWeight={'regular'} bgColor='#E4F0FC' py='1' px='2' borderRadius={'full'} cursor='pointer'>
                   <Text fontSize={'sm'} fontWeight={'bold'} >
                     Profile
