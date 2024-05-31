e9c2395802849f01cee0d072328677812cebf292
Dennis Sammy <dennissammy77@gmail.com>
2024-05-31 21:05:49 +0300

    Changes

--- src/components/providers/providers.js
+++ src/components/providers/providers.js
@@ -5,16 +5,19 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import {ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton} from '@clerk/clerk-react';
 
 export function Providers({ children }) {
   const queryClient = new QueryClient()
   return (
-    <ChakraProvider>
-        <QueryClientProvider client={queryClient}>
-          <UserProvider>
-            {children}
-          </UserProvider>
-        </QueryClientProvider>
-    </ChakraProvider>
+	<ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+		<ChakraProvider>
+			<QueryClientProvider client={queryClient}>
+				<UserProvider>
+					{children}
+				</UserProvider>
+			</QueryClientProvider>
+		</ChakraProvider>
+	</ClerkProvider>
   );
 }
