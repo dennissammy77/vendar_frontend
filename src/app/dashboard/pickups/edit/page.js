50ae78439a7a48e0077b5e57d62ea61697010387
Dennis Sammy <dennissammy77@gmail.com>
2024-06-03 20:25:23 +0300

    Changes

--- src/app/dashboard/pickups/edit/page.js
+++ src/app/dashboard/pickups/edit/page.js
@@ -9,6 +9,7 @@ import { useQuery } from '@tanstack/react-query';
 import { FETCH_PICKUP_DATA } from '@/app/api/pickup/route';
 import UPDATE_STORE_PICKUP_FORM from '@/components/ui/pickups/UPDATE_STORE_PICKUP_FORM';
 import FAILED_DATA_REQUEST from '@/components/ui/handlers/failed.data.error';
+import LOADING from '../../loading';
 
 
 function Page() {
@@ -42,25 +43,27 @@ function Page() {
         )
     }
     return (
-        <Box>
-            <Text fontWeight='bold' fontSize='32px'>Edit Pick Up Data</Text>
-            <Breadcrumb spacing='8px' separator={<CHEVRON_RIGHT_ICON color='gray.500' />}>
-                <BreadcrumbItem>
-                    <BreadcrumbLink href={`/dashboard/home/?uid=${USER_ID}`}>Home</BreadcrumbLink>
-                </BreadcrumbItem>
+        <Suspense fallback={<LOADING/>}>
+            <Box>
+                <Text fontWeight='bold' fontSize='32px'>Edit Pick Up Data</Text>
+                <Breadcrumb spacing='8px' separator={<CHEVRON_RIGHT_ICON color='gray.500' />}>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href={`/dashboard/home/?uid=${USER_ID}`}>Home</BreadcrumbLink>
+                    </BreadcrumbItem>
 
-                <BreadcrumbItem>
-                    <BreadcrumbLink href={`/dashboard/pickups/view/?uid=${USER_ID}&store_id=${STORE_ID}&pickup_id=${PICKUP_ID}`}>PickUps</BreadcrumbLink>
-                </BreadcrumbItem>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href={`/dashboard/pickups/view/?uid=${USER_ID}&store_id=${STORE_ID}&pickup_id=${PICKUP_ID}`}>PickUps</BreadcrumbLink>
+                    </BreadcrumbItem>
 
-                <BreadcrumbItem>
-                    <BreadcrumbLink isCurrentPage>{PICKUP_ID}</BreadcrumbLink>
-                </BreadcrumbItem>
-            </Breadcrumb>
-            <UPDATE_STORE_PICKUP_FORM PICKUP_DATA={PICKUP_DATA} USER_ID={USER_ID} PICKUP_ID={PICKUP_ID}/>
-            <Button variant={'ghost'} borderRadius={'md'} mt='2' w='full' onClick={(()=>{router.back()})} leftIcon={<DISCARD_ICON />}>Discard</Button>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink isCurrentPage>{PICKUP_ID}</BreadcrumbLink>
+                    </BreadcrumbItem>
+                </Breadcrumb>
+                <UPDATE_STORE_PICKUP_FORM PICKUP_DATA={PICKUP_DATA} USER_ID={USER_ID} PICKUP_ID={PICKUP_ID}/>
+                <Button variant={'ghost'} borderRadius={'md'} mt='2' w='full' onClick={(()=>{router.back()})} leftIcon={<DISCARD_ICON />}>Discard</Button>
 
-        </Box>
+            </Box>
+        </Suspense>
     )
 }
 
